f110fe8e184ab25976a75efd05955aadce3525c5
Lou1sM <oneillml@tcd.ie>
2021-03-08 11:38:11 +0000

    remove pseudo_label2 stuff, add cl args for different subject ids

--- har_cnn.py
+++ har_cnn.py
@@ -22,7 +22,6 @@ def display(latents_to_display):
     umapped_latents = umap.UMAP(min_dist=0,n_neighbors=30,n_components=2,random_state=42).fit_transform(latents_to_display.squeeze())
     utils.scatter_clusters(umapped_latents,labels=None,show=True)
 
-
 class StepDataset(data.Dataset):
     def __init__(self,x,y,device,window_size,step_size,pl_training,transforms=[]):
         self.device=device
@@ -51,6 +50,58 @@ class StepDataset(data.Dataset):
             window_var = sum([(item-mu) for item in self.window])/self.window_size
             if window_var < self.split_thresh: total_loss += window_var
         return total_loss
+
+class Enc(nn.Module):
+    def __init__(self):
+        super(Enc,self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1,4,(50,5),(2,1)),
+            nn.BatchNorm2d(4),
+            nn.LeakyReLU(0.3),
+            nn.MaxPool2d(2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(4,8,(40,3),(2,1)),
+            nn.LeakyReLU(0.3),
+            nn.BatchNorm2d(8),
+            nn.MaxPool2d(3),
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(8,16,(5,2),(1,1)),
+            nn.LeakyReLU(0.3),
+            nn.BatchNorm2d(16),
+            nn.MaxPool2d(2),
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(16,32,(3,1),(1,1)),
+            nn.LeakyReLU(0.3),
+            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2),
+        )
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        return out
+
+class Var_BS_MLP(nn.Module):
+    def __init__(self,input_size,hidden_size,output_size):
+        super(Var_BS_MLP,self).__init__()
+        self.fc1 = nn.Linear(input_size,hidden_size)
+        self.bn1 = nn.BatchNorm1d(hidden_size)
+        self.act1 = nn.LeakyReLU(0.3)
+        self.fc2 = nn.Linear(hidden_size,output_size)
+        #self.act2 = nn.Softmax()
+
+    def forward(self,x):
+        x = self.fc1(x)
+        if x.shape[0] != 1:
+            x = self.bn1(x)
+        x = self.act1(x)
+        x = self.fc2(x)
+        #x = self.act2(x)
+        return x
 
 class HARLearner():
     def __init__(self,dset,enc,dec,mlp,device,batch_size,num_classes):
@@ -108,27 +159,15 @@ class HARLearner():
         torch.save(self.enc.state_dict(),'enc_pretrained.pt')
         torch.save(self.dec.state_dict(),'dec_pretrained.pt')
 
-    def pseudo_label_train(self,mask,pseudo_labels,num_epochs,writer,pseudo_labels2='none',position_in_meta_loop=0):
+    def pseudo_label_train(self,mask,pseudo_labels,num_epochs,writer,position_in_meta_loop=0):
         self.enc.train()
         if isinstance(pseudo_labels,np.ndarray):
             pseudo_labels = torch.tensor(pseudo_labels)
-        if isinstance(pseudo_labels2,np.ndarray):
-            pseudo_labels2 = torch.tensor(pseudo_labels2)
         pseudo_label_dset = StepDataset(self.dset.x,pseudo_labels,device='cuda',window_size=self.dset.window_size,step_size=self.dset.step_size, pl_training=False)
         pseudo_label_dl = data.DataLoader(pseudo_label_dset,batch_sampler=data.BatchSampler(data.RandomSampler(pseudo_label_dset),self.batch_size,drop_last=True),pin_memory=False)
-        if pseudo_labels2=='none':
-            pseudo_label_dset2 = StepDataset(self.dset.x,pseudo_labels,device='cuda',window_size=self.dset.window_size,step_size=self.dset.step_size, pl_training=False)
-            pseudo_label_dl2 = data.DataLoader(pseudo_label_dset,batch_sampler=data.BatchSampler(data.RandomSampler(pseudo_label_dset),self.batch_size,drop_last=True),pin_memory=False)
-        else:
-            pseudo_label_dset2 = StepDataset(self.dset.x,torch.tensor(pseudo_labels2),device='cuda',window_size=self.dset.window_size,step_size=self.dset.step_size, pl_training=False)
-            pseudo_label_dl2 = data.DataLoader(pseudo_label_dset2,batch_sampler=data.BatchSampler(data.RandomSampler(pseudo_label_dset2),self.batch_size,drop_last=True),pin_memory=False)
-            assert (pseudo_labels==pseudo_labels2).all()
-        to_iter = zip(pseudo_label_dl,pseudo_label_dl2)
         all_pseudo_label_losses = []
         start_indexing_at = position_in_meta_loop*num_epochs*len(pseudo_label_dl)
         for epoch in range(num_epochs):
-            epoch_rec_loss = 0
-            epoch_pseudo_label_loss = 0
             epoch_loss = 0
             epoch_rec_losses = []
             epoch_pseudo_label_losses = []
@@ -145,11 +184,6 @@ class HARLearner():
                         pseudo_label_pred = self.mlp(latents_to_pseudo_label_train[:,:,0,0])
                     except ValueError: set_trace()
                     pseudo_label_loss = self.pseudo_label_lf(pseudo_label_pred,yb[batch_mask])
-                    if pseudo_labels2 != 'none':
-                        try:
-                            test_loss = self.pseudo_label_lf(pseudo_label_pred,torch.tensor(pseudo_labels2[idx][batch_mask],device='cuda'))
-                        except: set_trace()
-                        assert test_loss == pseudo_label_loss
                 else:
                     pseudo_label_loss = torch.tensor(0,device=self.device)
                 if not batch_mask.all():
@@ -183,13 +217,7 @@ class HARLearner():
             else:
                 count += 1
             if count > 4: break
-            #print(f'Rec Loss: {epoch_rec_loss}\tPseudo_label loss: {epoch_pseudo_label_loss}\t Total:{epoch_loss}')
             assert (pseudo_label_dset.x==self.dset.x).all()
-            #plt.plot(epoch_pseudo_label_losses, label='pseudo_label')
-            #plt.plot(epoch_rec_losses, label='rec')
-            #plt.plot(epoch_losses, label='total')
-            #plt.legend()
-            #plt.show(block=False)
         return all_pseudo_label_losses
 
     def train_meta_loop(self,num_pre_epochs,num_meta_epochs,num_pseudo_label_epochs,prob_thresh,selected_acts,exp_dir):
@@ -235,7 +263,7 @@ class HARLearner():
                 print('Sames:', sum(mask2), sum(mask2)/len(new_pred_labels))
                 mask = mask*mask2
                 assert (new_pred_labels[mask]==old_pred_labels[mask]).all()
-                self.pseudo_label_train(mask=mask,pseudo_labels2=old_pred_labels,pseudo_labels=new_pred_labels,num_epochs=num_pseudo_label_epochs,writer=writer,position_in_meta_loop=epoch_num)
+                self.pseudo_label_train(mask=mask,pseudo_labels=new_pred_labels,num_epochs=num_pseudo_label_epochs,writer=writer,position_in_meta_loop=epoch_num)
             else:
                 self.pseudo_label_train(mask=mask,pseudo_labels=new_pred_labels,num_epochs=num_pseudo_label_epochs,writer=writer)
             print('translating labelling')
@@ -246,80 +274,25 @@ class HARLearner():
             print('Masked Counts:',mask_counts)
             print('Latent accuracy:', utils.accuracy(new_pred_labels,self.dset.y))
             print('Masked Latent accuracy:', utils.accuracy(new_pred_labels[mask],self.dset.y[mask]),mask.sum())
-            rand_idxs = np.array([15,1777,1982,9834,11243,25,7777,5982,5834,41203,250,7717,5912,5134,41843])
+            rand_idxs = np.array([15,1777,1982,9834,11243,25,7777,5982,5834,250,7717,5912,5134])
             np_gt_labels = self.dset.y.detach().cpu().numpy().astype(int)
             for action_num in np.unique(np_gt_labels):
                 action_preds = new_pred_labels[np_gt_labels==action_num]
                 action_name = selected_acts[action_num]
                 num_correct = (action_preds==action_num).sum()
                 total_num = len(action_preds)
-                print(f"{action_name}: {num_correct/total_num} ({num_correct}/{total_num})")
+                print(f"{action_name}: {round(num_correct/total_num,3)} ({num_correct}/{total_num})")
             print('GT:',self.dset.y[rand_idxs].int().tolist())
             print('Old:',old_pred_labels[rand_idxs])
             print('New:',new_pred_labels[rand_idxs])
             old_pred_labels = copy.deepcopy(new_pred_labels)
         # Save models
         if ARGS.save:
-            with open('hmm_model.pkl', 'wb') as f: pickle.dump(model,f)
-            utils.torch_save({'enc':self.enc,'dec':self.dec,'mlp':self.mlp},exp_dir,'har_learner.pt')
+            utils.torch_save({'enc':self.enc,'dec':self.dec,'mlp':self.mlp},exp_dir,f'har_learner{ARGS.exp_name}.pt')
+            with open(os.path.join(exp_dir,f'HMM{ARGS.exp_name}.pkl', 'wb')) as f: pickle.dump(model,f)
 
-
-class Enc(nn.Module):
-    def __init__(self):
-        super(Enc,self).__init__()
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1,4,(50,5),(2,1)),
-            nn.BatchNorm2d(4),
-            nn.LeakyReLU(0.3),
-            nn.MaxPool2d(2)
-        )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(4,8,(40,3),(2,1)),
-            nn.LeakyReLU(0.3),
-            nn.BatchNorm2d(8),
-            nn.MaxPool2d(3),
-        )
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(8,16,(5,2),(1,1)),
-            nn.LeakyReLU(0.3),
-            nn.BatchNorm2d(16),
-            nn.MaxPool2d(2),
-        )
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(16,32,(3,1),(1,1)),
-            nn.LeakyReLU(0.3),
-            nn.BatchNorm2d(32),
-            nn.MaxPool2d(2),
-        )
-    def forward(self,x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        return out
-
-class Var_BS_MLP(nn.Module):
-    def __init__(self,input_size,hidden_size,output_size):
-        super(Var_BS_MLP,self).__init__()
-        self.fc1 = nn.Linear(input_size,hidden_size)
-        self.bn1 = nn.BatchNorm1d(hidden_size)
-        self.act1 = nn.LeakyReLU(0.3)
-        self.fc2 = nn.Linear(hidden_size,output_size)
-        #self.act2 = nn.Softmax()
-
-    def forward(self,x):
-        x = self.fc1(x)
-        if x.shape[0] != 1:
-            x = self.bn1(x)
-        x = self.act1(x)
-        x = self.fc2(x)
-        #x = self.act2(x)
-        return x
-
-
-def train(args):
+def train(args,subj_ids):
     # Make dataset
-    subj_ids = [101,102,103,104,105,106,107,108,109] if args.all_subjs else [101]
 
     action_name_dict = {1:'lying',2:'sitting',3:'standing',4:'walking',5:'running',6:'cycling',7:'Nordic walking',9:'watching TV',10:'computer work',11:'car driving',12:'ascending stairs',13:'descending stairs',16:'vacuum cleaning',17:'ironing',18:'folding laundry',19:'house cleaning',20:'playing soccer',24:'rope jumping'}
     x = np.concatenate([np.load(f'PAMAP2_Dataset/np_data/subject{subj_id}.npy') for subj_id in subj_ids])
@@ -386,16 +359,18 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size',type=int,default=128)
     parser.add_argument('--dec_lr',type=float,default=1e-3)
     parser.add_argument('--enc_lr',type=float,default=1e-3)
-    parser.add_argument('--exp_name',type=str)
+    parser.add_argument('--exp_name',type=str,default="")
     parser.add_argument('--load_pretrained',action='store_true')
     parser.add_argument('--mlp_lr',type=float,default=1e-3)
     parser.add_argument('--noise',type=float,default=1.)
     parser.add_argument('--num_meta_epochs',type=int,default=30)
     parser.add_argument('--num_pre_epochs',type=int,default=5)
     parser.add_argument('--num_pseudo_label_epochs',type=int,default=5)
+    parser.add_argument('--parallel',action='store_true')
     parser.add_argument('--prob_thresh',type=float,default=.95)
     parser.add_argument('--save','-s',action='store_true')
     parser.add_argument('--step_size',type=int,default=5)
+    parser.add_argument('--subj_ids',type=int,nargs='+',default=5)
     parser.add_argument('--test','-t',action='store_true')
     parser.add_argument('--window_size',type=int,default=512)
     ARGS = parser.parse_args()
@@ -403,4 +378,17 @@ if __name__ == "__main__":
     if ARGS.test and ARGS.save:
         print("Shouldn't be saving for a test run"); sys.exit()
     if ARGS.test: ARGS.num_meta_epochs = 2
-    train(ARGS)
+    all_possible_ids = [101,102,103,104,105,106,107,108,109]
+    bad_ids = [x for x in ARGS.subj_ids if x not in all_possible_ids]
+    if len(bad_ids) > 0:
+        print("You have specified non-existent ids: {bad_ids}"); sys.exit()
+    if ARGS.all_subjs:
+        ARGS.subj_ids=all_possible_ids
+    if ARGS.parallel:
+        train(ARGS,subj_ids=ARGS.subj_ids)
+    else:
+        orig_name = ARGS.exp_name
+        for subj_id in ARGS.subj_ids:
+            print(f"Training and predicting on id {subj_id}")
+            ARGS.exp_name = f"{orig_name}s{subj_id}"
+            train(ARGS,subj_ids=[subj_id])
