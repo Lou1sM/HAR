0f740fd723a74716cd9c598e793cbd5a0d8f19d7
Louis <oneillml@tcd.ie>
2021-04-26 13:09:31 +0000

    bash script to download data

---
+++ prep.sh
@@ -0,0 +1,4 @@
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00231/PAMAP2_Dataset.zip
+unzip PAMAP2_Dataset.zip
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip
+unzip UCI\ HAR\ Dataset.zip
