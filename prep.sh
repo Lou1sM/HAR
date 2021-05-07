133b7a757e6693cb7f3fa398fb8bc6cf8de7393b
Lou1sM <oneillml@tcd.ie>
2021-05-07 16:43:29 +0100

    add wisdm to prep bash script

--- prep.sh
+++ prep.sh
@@ -1,4 +1,11 @@
-wget https://archive.ics.uci.edu/ml/machine-learning-databases/00231/PAMAP2_Dataset.zip
-unzip PAMAP2_Dataset.zip
-wget https://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip
-unzip UCI\ HAR\ Dataset.zip
+#wget https://archive.ics.uci.edu/ml/machine-learning-databases/00227/PAMAP2_Dataset.zip
+#unzip PAMAP2_Dataset.zip
+#python convert_data_to_np.py PAMAP
+#mkdir UCI2
+#cd UCI2
+#wget http://archive.ics.uci.edu/ml/machine-learning-databases/00341/HAPT%20Data%20Set.zip
+#unzip UCI\ HAR\ Dataset.zip
+#cd ..
+#python convert_data_to_np.py UCI
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00507/wisdm-dataset.zip
+#python convert_data_to_np.py WISDM
